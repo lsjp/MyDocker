ad001289e892d440268c036d16284cbe8e2b070a
peter <zealotphoenix@hotmail.com>
2017-04-28 16:55:07 +0800

    change router [delete /]

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,8 +1,8 @@
 var indexRouter = require('express').Router();
 
 module.exports = function() {
-    indexRouter.use('/', require('./welcome')());
-    indexRouter.use('/booking', require('./booking')());
-    indexRouter.use('/user', require('./user')());
+    indexRouter.use('', require('./welcome')());
+    indexRouter.use('booking', require('./booking')());
+    indexRouter.use('user', require('./user')());
     return indexRouter;
 };
