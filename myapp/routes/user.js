480daca6f49316e8920a0a8e751789c23b07df13
peter <zealotphoenix@hotmail.com>
2017-05-02 11:07:49 +0800

    reset nginx server.conf [for match location ^~ /wx/]

--- myapp/routes/user.js
+++ myapp/routes/user.js
@@ -3,11 +3,13 @@ var weixinuser = require('../components/model/weixinuser');
 
 module.exports = function () {
     userRouter.get('/', function (req, res) {
-        weixinuser.search("select * from user");
+        console.log(weixinuser.search("select * from user"));
+        res.send('hello user')
     });
 
     userRouter.get('/add', function (req, res) {
         weixinuser.insert();
+        res.send('add')
     });
     return userRouter;
 };
